063460e11b9a60a894cf43a0b6225d78ebe1c318
Nick Bohannan <nbohannan@gmail.com>
2018-10-23 15:45:32 -0500

    first commit

--- src/com/example/tutorial/ServletExample.java
+++ src/com/example/tutorial/ServletExample.java
@@ -29,11 +29,22 @@ public class ServletExample extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
 		
-		String firstname = request.getParameter("firstname");
-		String lastname = request.getParameter("lastname");
-		out.println(firstname + lastname);
+		if (request.getParameter("firstname") == null || request.getParameter("lastname") == null) {
+			getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+			
+			
+		}
+		
+		
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		
+		request.setAttribute("firstname", firstName);
+		request.setAttribute("lastname", lastName);
+		
+		getServletContext().getRequestDispatcher("/output.jsp").forward(request, response);
 		
 	}
 
